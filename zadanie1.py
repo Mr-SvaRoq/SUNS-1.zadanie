e62065188cd80110d9c07ba0fb74e1a2914d8757
Mr-SvaRoq <bibo.dnt.159@gmail.com>
2019-10-17 18:21:31 +0200

    do not remember

--- zadanie1.py
+++ zadanie1.py
@@ -1,68 +1,3 @@
-
-# def my_function():
-#   print("Hello from a function")
-
-# class Car:
-#
-#     def __init__(self, speed=0):
-#         self.speed = speed
-#         self.odometer = 0
-#         self.time = 0
-#
-#     def say_state(self):
-#         print("I'm going {} kph!".format(self.speed))
-#
-#     def accelerate(self):
-#         self.speed += 5
-#
-#     def brake(self):
-#         self.speed -= 5
-#
-#     def step(self):
-#         self.odometer += self.speed
-#         self.time += 1
-#
-#     def average_speed(self):
-#         if self.time != 0:
-#             return self.odometer / self.time
-#         else:
-#             pass
-
-    # data_reader = DataReader();
-    # my_car = Car()
-    # print("I'm a car!")
-    # while True:
-    #     action = input("What should I do? [A]ccelerate, [B]rake, "
-    #              "show [O]dometer, or show average [S]peed?").upper()
-    #     if action not in "ABOS" or len(action) != 1:
-    #         print("I don't know how to do that")
-    #         continue
-    #     if action == 'A':
-    #         my_car.accelerate()
-    #     elif action == 'B':
-    #         my_car.brake()
-    #     elif action == 'O':
-    #         print("The car has driven {} kilometers".format(my_car.odometer))
-    #     elif action == 'S':
-    #         print("The car's average speed was {} kph".format(my_car.average_speed()))
-    #     my_car.step()
-    #     my_car.say_state()
-
-# class DataReader:
-#     import csv
-#
-#     with open('data_krajiny.csv', mode='r') as csv_file:
-#         csv_reader = csv.reader(csv_file, delimiter=',')
-#         line_count = 0
-#         for row in csv_reader:
-#             if line_count == 0:
-#                 print(f'Column names are {", ".join(row)}')
-#                 line_count += 1
-#             else:
-#                 print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#                 line_count += 1
-#         print(f'Processed {line_count} lines.')S
-
 import csv
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -75,8 +10,9 @@ if __name__ == '__main__':
     stringDictionary = {}
     stringDictionaryKey = 1
     countryDictionary = []
+    regionDictionary = []
 
-    print('\n---------NACITANIE A KONVERTOVANIE DAT----------')
+    print('\n---------NACITANIE A KONVERTOVANIE DAT-------ECH---')
     with open('data_krajiny.csv', mode='r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -85,14 +21,23 @@ if __name__ == '__main__':
             if line_count == 31:
                 break
             if line_count == 0:
+                print(row)
                 line_count += 1
             else:
                 newLineOfArray = []
                 counter = -1
                 for x in row:
                     counter += 1
-                    if counter == 10 or counter == 0:
+                    if counter == 10:
                         continue
+                    # na zapamatanie regionu
+                    if counter == 1:
+                        regionDictionary.append(x)
+                    # na zapamatanie country
+                    if counter == 0:
+                        countryDictionary.append(x)
+                        continue
+                    # ak je cislo, da, ak nie konvertuje
                     try:
                         floatValue = float(x)
                         newLineOfArray.append(floatValue)
@@ -108,16 +53,14 @@ if __name__ == '__main__':
                             newLineOfArray.append(stringDictionary[x])
                 allData.append(newLineOfArray)
                 line_count += 1
+    print(countryDictionary)
     for row in allData:
         print(row)
 
     print('\n---------STRING and REGION(2. STLPEC) DICTIONARY----------')
     print(stringDictionary)
-    # X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-    # kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-
-    # transformer = Normalizer().fit(allData)
-    # print(transformer.transform(allData))
+    print('\n---------ONLY REGION(2. STLPEC) DICTIONARY----------')
+    print(regionDictionary)
 
     with open("allDataConvert.csv", 'w', newline='') as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=',')
@@ -138,32 +81,52 @@ if __name__ == '__main__':
     print(euclideanAllData)
     with open("euclideanAllData.csv", 'w', newline='') as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=',')
-        # for row in euclideanAllData:
-            # csvWriter.writerow(row)
         csvWriter.writerows(euclideanAllData)
-    # overenie normalizacie ?
-    # index = 3
-    # print(countryDictionary[index])
-    # print(scaledAllData[index])
 
     print('\n---------K MEANS----------')
-    kmeans = KMeans(n_clusters=5).fit(scaledAllData)
-    #kolko chcem skupin/klastrov
+    # pocet regionov a tak, to by bolo fajn opravit, ze dam pocet klastrov ako je regionov a ptm porovnam, ci to sedi s originialnimi datami
+    kmeans = KMeans(n_clusters=10).fit(scaledAllData)
+    # print(kmeans.cluster_centers_)
+
+
+    # kolko chcem skupin/klastrov
+
+    with open("K-means.csv", 'w', newline='') as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+        csvWriter.writerow(countryDictionary)
+        csvWriter.writerow(kmeans.labels_)
+        csvWriter.writerow(regionDictionary)
     set(kmeans.labels_)
     print(kmeans.labels_)
     print(set(kmeans.labels_))
 
-    # counter = 0
-    # for k in kmeans.labels_:
-    #     print(counter)
-    #     counter +=1
-    # print(kmeans.cluster_centers_)
 
     print('\n---------DB SCAN----------')
-    dbscan = DBSCAN(eps=1.5, min_samples=2).fit(scaledAllData)
+    dbscan = DBSCAN(eps=1.68, min_samples=1).fit(scaledAllData)
     print(dbscan.labels_)
     print(set(dbscan.labels_))
+    with open("DBscan.csv", 'w', newline='') as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+        csvWriter.writerow(countryDictionary)
+        csvWriter.writerow(dbscan.labels_)
+        csvWriter.writerow(regionDictionary)
 
+    print('\n---------K MEANS - Cluster Centers----------')
+    # dataIndex = 0
+    with open("euclideanClusterCenters.csv", 'w', newline='') as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+
+        for dataIndex in range(0, 10):
+            for data in scaledAllData:
+                foo = [kmeans.cluster_centers_[dataIndex], data]
+                # euclideanClusterCenters = euclidean_distances(kmeans.cluster_centers_[0], scaledAllData[0])
+                euclideanClusterCenters = euclidean_distances(foo, foo)
+                # print(euclideanClusterCenters)
+            csvWriter.writerows(euclideanClusterCenters)
+
+    with open("cluster_centers_.csv", 'w', newline='') as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+        csvWriter.writerows(kmeans.cluster_centers_)
 
 # https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
 # https://scikit-learn.org/stable/datasets/index.html
